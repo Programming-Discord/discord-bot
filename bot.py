58e914160f02f66ca89fb4f6f332a1ddde9001d1
Shade Gladden <shadeyg56@gmail.com>
2018-07-31 11:04:13 -0500

    add mongodb connection and eval command

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import time
 import os
+from motor.motor_asyncio import AsyncIOMotorClient
+import asyncio
 
 #got the Token this way because Python was being stupid and wouldn't import another file and I was too lazy to figure it out XD
 bot = commands.Bot(command_prefix="~")
@@ -16,6 +18,8 @@ async def on_ready():
 	print('Author: The Programming Discord :D')
 	print("ID: {}".format(bot.user.id))
 	print('DV: {}'.format(discord.__version__))
+	mongo = AsyncIOMotorClient(os.environ.get("mongo"))
+	bot.db = mongo.discord-bot
 
 @bot.command()
 async def online(ctx):
@@ -28,6 +32,91 @@ async def ping(ctx):
     t2 = time.perf_counter()
     ping = round((t2-t1)*1000)
     await message.edit(content=f":ping_pong: Pong! `{ping}`ms")
+
+def paginate(text: str):
+    '''Simple generator that paginates text.'''
+    last = 0
+    pages = []
+    for curr in range(0, len(text)):
+        if curr % 1980 == 0:
+            pages.append(text[last:curr])
+            last = curr
+            appd_index = curr
+    if appd_index != len(text) - 1:
+        pages.append(text[last:curr])
+    return list(filter(lambda a: a != '', pages))
+
+def cleanup_code(content):
+    '''Automatically removes code blocks from the code.'''
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+    return content.strip('` \n')
+
+
+
+@bot.command(name='eval', hidden=True)
+@bot.is_owner()
+async def _eval(ctx, *, body):
+    """Evaluates python code"""
+    env = {
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+        '_': bot._last_result,
+        'source': inspect.getsource,
+        'session': bot.session
+    }
+
+    env.update(globals())
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+    err = out = None
+    to_compile = f'async def func(): \n{textwrap.indent(body, "  ")}'
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+        return await ctx.message.add_reaction('\u2049')
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        if ret is None:
+            if value:
+                try:
+                    out = await ctx.send(f'```py\n{value}\n```')
+                except:
+                    paginated_text = paginate(value)
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+        else:
+            bot._last_result = ret
+            try:
+                out = await ctx.send(f'```py\n{value}{ret}\n```')
+            except:
+                paginated_text = paginate(f"{value}{ret}")
+                for page in paginated_text:
+                    if page == paginated_text[-1]:
+                        out = await ctx.send(f'```py\n{page}\n```')
+                        break
+                    await ctx.send(f'```py\n{page}\n```')
+    if out:
+        await ctx.message.add_reaction('\u2705')  # tick
+    elif err:
+        await ctx.message.add_reaction('\u2049')  # x
+    else:
+        await ctx.message.add_reaction('\u2705')
 
 
 if __name__ == "__main__":
