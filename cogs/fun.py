5028b0db278a2d24b943e0e9fad95ec400730ed8
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-08-06 12:00:06 -0500

    this is so sad, alexa play despacito

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import aiohttp
 import random
+import asyncio
 
 ballans = [
     "It is certain",
@@ -51,6 +52,21 @@ class Fun():
 		embed = discord.Embed(title="Kitty!", color=discord.Color.blue())
 		embed.set_image(url=json["url"])
 		await ctx.send(embed=embed)
+		
+	async def on_message(self, message):
+		if "alexa play despacito" in message.content:
+			lyrics = ["Ay, ¡Fonsi! ¡D.Y.!\nOhhh, oh, no, oh, no, oh'\nHey, yeah!\nDididiri Daddy, go!", 
+				  "Sí, sabes que ya llevo un rato mirándote\nTengo que bailar contigo hoy\n(¡D.Y.!) Vi que tu mirada ya estaba llamándome\nMuéstrame el camino que yo voy", 
+				  "¡Oh!\nTú, tú eres el imán y yo soy el metal\nMe voy acercando y voy armando el plan\nSólo con pensarlo se acelera el pulso (oh, yeah!)\nYa, ya me está gustando más de lo normal\nTodos mis sentidos van pidiendo más\nEsto hay que tomarlo sin ningún apuro", 
+				  "**DES PA CITO**", 
+				  "Quiero respirar tu cuello despacito\nDeja que te diga cosas al oído\nPara que te acuerdes si no estás conmigo", 
+				  "**DES PA CITO**", 
+				  "Quiero desnudarte a besos despacito\nFirmo en las paredes de tu laberinto\nY hacer de tu cuerpo todo un manuscrito\n(Sube, sube, sube, Sube, sube)"]
+			await message.channel.send("`Now Playing: Luis Fonsi - Despacito ft. Daddy Yankee ⚪────────────── ◄◄⠀▶⠀►►⠀ 00:00 / 4:42 ⠀ ───○ 🔊⚙️`
+			for line in lyrics:
+				await asyncio.sleep(2.5)
+				await message.channel.send(line)
+		
 
 def setup(bot):
 	bot.add_cog(Fun(bot))
