c9e063231544eacdfc9dc824a494ff406de8c400
Shade Gladden <shadeyg56@gmail.com>
2018-08-15 12:23:32 -0500

    users I add to the trusted users can now use eval

--- bot.py
+++ bot.py
@@ -23,23 +23,27 @@ async def get_prefix(bot, message):
 #got the Token this way because Python was being stupid and wouldn't import another file and I was too lazy to figure it out XD
 bot = commands.Bot(command_prefix=get_prefix)
 bot._last_result = None
-cogs = {"fun", "ranks", "config"}
 
-def is_owner():
-	return commands.check(lambda ctx: ctx.author.id == 300396755193954306)
+cogs = {"fun", "ranks", "config"}
 
 @bot.event
 async def on_ready():
-	print('------------------------------------')
-	print('THE BOT IS ONLINE')
-	print('------------------------------------')
-	print("Name: {}".format(bot.user.name))
-	print('Author: The Programming Discord :D')
-	print("ID: {}".format(bot.user.id))
-	print('DV: {}'.format(discord.__version__))
-	mongo = AsyncIOMotorClient(os.environ.get("mongo"))
-	bot.db = mongo.programmingdiscordbot
-	bot.session = aiohttp.ClientSession(loop=bot.loop)
+    print('------------------------------------')
+    print('THE BOT IS ONLINE')
+    print('------------------------------------')
+    print("Name: {}".format(bot.user.name))
+    print('Author: The Programming Discord :D')
+    print("ID: {}".format(bot.user.id))
+    print('DV: {}'.format(discord.__version__))
+    mongo = AsyncIOMotorClient(os.environ.get("mongo"))
+    bot.db = mongo.programmingdiscordbot
+    bot.session = aiohttp.ClientSession(loop=bot.loop)
+
+def is_owner():
+    return commands.check(lambda ctx: ctx.author.id == 300396755193954306)
+
+
+
 
 #Welcome and leave message handling
 @bot.event
@@ -115,68 +119,89 @@ def cleanup_code(content):
 
 
 @bot.command(name='eval', hidden=True)
-#default bot.is_owner() wasn't working so I just wrote a custom function
-@is_owner()
 async def _eval(ctx, *, body):
     """Evaluates python code"""
-    env = {
-        'ctx': ctx,
-        'channel': ctx.channel,
-        'author': ctx.author,
-        'guild': ctx.guild,
-        'message': ctx.message,
-        '_': bot._last_result,
-        'source': inspect.getsource
-    }
+    user = await bot.db.trusted.find_one({"_id": ctx.author.id})
+    if user:
+        env = {
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': bot._last_result,
+            'source': inspect.getsource
+        }
 
-    env.update(globals())
-    body = cleanup_code(body)
-    stdout = io.StringIO()
-    err = out = None
-    to_compile = f'async def func(): \n{textwrap.indent(body, "  ")}'
-    try:
-        exec(to_compile, env)
-    except Exception as e:
-        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-        return await ctx.message.add_reaction('\u2049')
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
-            ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-    else:
-        value = stdout.getvalue()
-        if ret is None:
-            if value:
+        env.update(globals())
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+        err = out = None
+        to_compile = f'async def func(): \n{textwrap.indent(body, "  ")}'
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.message.add_reaction('\u2049')
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
+                        out = await ctx.send(f'```py\n{value}\n```')
+                    except:
+                        paginated_text = paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                out = await ctx.send(f'```py\n{page}\n```')
+                                break
+                            await ctx.send(f'```py\n{page}\n```')
+            else:
+                bot._last_result = ret
                 try:
-                    out = await ctx.send(f'```py\n{value}\n```')
+                    out = await ctx.send(f'```py\n{value}{ret}\n```')
                 except:
-                    paginated_text = paginate(value)
+                    paginated_text = paginate(f"{value}{ret}")
                     for page in paginated_text:
                         if page == paginated_text[-1]:
                             out = await ctx.send(f'```py\n{page}\n```')
                             break
                         await ctx.send(f'```py\n{page}\n```')
+        if out:
+            await ctx.message.add_reaction('\u2705')  # tick
+        elif err:
+            await ctx.message.add_reaction('\u2049')  # x
         else:
-            bot._last_result = ret
-            try:
-                out = await ctx.send(f'```py\n{value}{ret}\n```')
-            except:
-                paginated_text = paginate(f"{value}{ret}")
-                for page in paginated_text:
-                    if page == paginated_text[-1]:
-                        out = await ctx.send(f'```py\n{page}\n```')
-                        break
-                    await ctx.send(f'```py\n{page}\n```')
-    if out:
-        await ctx.message.add_reaction('\u2705')  # tick
-    elif err:
-        await ctx.message.add_reaction('\u2049')  # x
+            await ctx.message.add_reaction('\u2705')
     else:
-        await ctx.message.add_reaction('\u2705')
+        await ctx.send("You aren't a trusted user")
 
+@bot.command()
+@is_owner()
+async def add_trusted(ctx, user: discord.Member):
+    x = await bot.db.trusted.find_one({"_id": user.id})
+    if not x:
+        await bot.db.trusted.insert_one({"_id": user.id, "name": f"{user.name}#{user.discriminator}"})
+        await ctx.send(f"Added {user.name}#{user.discriminator} to the trusted users")
+    else:
+        await ctx.send('That user is already trusted')
+
+@bot.command()
+@is_owner()
+async def remove_trusted(ctx, user: discord.Member):
+    x = await bot.db.trusted.find_one({"_id": user.id})
+    if x:
+        await bot.db.trusted.delete_many({"name": f"{user.name}#{user.discriminator}"})
+        await ctx.send(f"Removed {user.name}#{user.discriminator} from the trusted users")
+    else:
+        await ctx.send('That user is not even trusted.')
 
 
 if __name__ == "__main__":
